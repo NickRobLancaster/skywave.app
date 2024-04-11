39d4bc5e84822fe41d8c69767a0e8ea1cafeb823
NickRobLancaster <92408834+SkywaveNick@users.noreply.github.com>
2024-04-10 17:55:23 -0700

    fixed spacing on spiff adders

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -598,9 +598,9 @@ const spiffTodayHoverLeave = () => {
           <div
             class="h-1/3 bg-base-100 flex-col grid grid-cols-2 border-t border-t-slate-400 divide-x divide-slate-400"
           >
-            <div class="p-2">
-              <div class="flex flex-row">
-                <div>Spiffs - Week</div>
+            <div class="h-full overflow-y-auto hide-scroll">
+              <div class="flex flex-row items-center p-2">
+                <div class="">Spiffs - Week</div>
                 <button
                   @click="spiffAdderWeek"
                   class="ml-auto btn btn-xs border border-slate-400"
@@ -610,7 +610,7 @@ const spiffTodayHoverLeave = () => {
               </div>
               <table class="table table-zebra table-xs">
                 <!-- head -->
-                <thead>
+                <thead class="sticky top-0 bg-base-100">
                   <tr>
                     <th>Rep</th>
                     <th>Spiff</th>
@@ -653,17 +653,16 @@ const spiffTodayHoverLeave = () => {
                   </tr>
                 </tbody>
               </table>
-              <div
-                v-if="spiffsWeek.length === 0"
-                class="bg-blue-500 p-5 rounded text-white text-center"
-              >
-                No Spiffs This Week Yet
+              <div v-if="spiffsWeek.length === 0" class="p-2">
+                <div class="bg-blue-500 p-5 rounded text-white text-center">
+                  No Spiffs This Week Yet
+                </div>
               </div>
             </div>
 
-            <div class="p-2">
-              <div class="flex flex-row">
-                <div>Spiffs - Today</div>
+            <div class="h-full overflow-y-auto hide-scroll">
+              <div class="flex flex-row items-center p-2">
+                <div class="">Spiffs - Today</div>
                 <button
                   @click="spiffAdderToday"
                   class="ml-auto btn btn-xs border border-slate-400"
@@ -673,13 +672,13 @@ const spiffTodayHoverLeave = () => {
               </div>
               <table class="table table-zebra table-xs">
                 <!-- head -->
-                <thead>
+                <thead class="sticky top-0 bg-base-100">
                   <tr>
                     <th>Rep</th>
                     <th>Spiff</th>
                     <th
-                      v-if="spiffTodayHovered && spiffsToday.length !== 0"
                       class="text-center"
+                      v-if="spiffTodayHovered && spiffsToday.length !== 0"
                     >
                       <font-awesome-icon icon="fa-solid fa-trash" />
                     </th>
@@ -716,12 +715,10 @@ const spiffTodayHoverLeave = () => {
                   </tr>
                 </tbody>
               </table>
-
-              <div
-                v-if="spiffsToday.length === 0"
-                class="bg-blue-500 p-5 rounded text-white text-center"
-              >
-                No Spiffs Today Yet
+              <div v-if="spiffsToday.length === 0" class="p-2">
+                <div class="bg-blue-500 p-5 rounded text-white text-center">
+                  No Spiffs Today Yet
+                </div>
               </div>
             </div>
           </div>
@@ -730,3 +727,9 @@ const spiffTodayHoverLeave = () => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.hide-scroll::-webkit-scrollbar {
+  display: none;
+}
+</style>
