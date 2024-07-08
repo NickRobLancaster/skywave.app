fae4ba4689ed6b30cddd0cbb03450af82648d61a
NickRobLancaster <92408834+SkywaveNick@users.noreply.github.com>
2024-07-08 16:10:05 -0700

    home page added background image brian wanted of the contact file

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,31 +3,22 @@
 <template>
   <!-- HERO -->
   <div
-    class="h-screen flex flex-col justify-center items-center gap-10 p-5 bg-gradient-to-b from-blue-900 to-blue-400 text-white"
+    class="pt-96 pb-24 flex flex-col justify-center items-center gap-10 p-5 bg-gradient-to-b from-blue-900 to-blue-400 text-white"
   >
     <h1 class="text-5xl font-bold text-center">Skywave CRM</h1>
     <p class="text-2xl text-center">
-      The work OS that lets you shape workflows, YOUR WAY.
+      It's Time to Streamline Your Business Operations
     </p>
-    <button
+    <!-- <button
       class="active:animate-press bg-blue-500 text-white rounded-full px-4 py-3 shadow-md"
     >
       Get Started
-    </button>
+    </button> -->
+
+    <div
+      class="rounded-lg overflow-clip border-slate-300 border-4 md:w-11/12 mt-52"
+    >
+      <img src="../../public/images/sw-v2-contact-file.png" alt="" />
+    </div>
   </div>
-
-  <!-- FEATURE GLANCE - GIFS -->
-  <div class="bg-red-500 h-screen"></div>
-
-  <!-- TRUSTED BY - LIST OF COMPANIES -->
-  <div class="bg-orange-500 h-screen"></div>
-
-  <!-- TAG LINE - Step into a world of seamless work experiences powered by a suite of products designed to flex and scale to your way of working -->
-  <div class="bg-yellow-500 h-screen"></div>
-
-  <!-- RATINGS - Captera, ETC -->
-  <div class="bg-green-500 h-screen"></div>
-
-  <!-- TAG LINE - The work OS that lets you shape workflows, YOUR WAY. Boost your team's alignment, efficiency, and productivy by customizing any workflow to fit your needs - ANOTHER GIF showing summary -->
-  <div class="bg-blue-500 h-screen"></div>
 </template>
